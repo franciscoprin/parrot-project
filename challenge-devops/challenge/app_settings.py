c7ab0f3fab4bd7d78b3652d0b91cb247468c985d
Francisco Prin <franciscoprin@gmail.com>
2024-07-24 12:23:56 -0600

    Skip database connection check if DJANGO_ENV is set to testing

--- challenge-devops/challenge/app_settings.py
+++ challenge-devops/challenge/app_settings.py
@@ -6,17 +6,15 @@ APP_VERSION = env('APP_VERSION', default='N/A')
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# Determine the environment
-DJANGO_ENV = env('DJANGO_ENV', default='development')
 
 # Default database settings
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB', default=None),
-        'USER': env('POSTGRES_USER', default=None),
-        'PASSWORD': env('POSTGRES_PASSWORD', default=None),
-        'HOST': env('POSTGRES_HOST', default=None),
-        'PORT': env('POSTGRES_PORT', default=None),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
